c4b1565cde383865f1503225f43cca53da2ddec8
john <johnthomas222000@gmail.com>
2023-04-11 20:18:54 +0530

    working on product container

--- src/Components/Dashboard.tsx
+++ src/Components/Dashboard.tsx
@@ -4,7 +4,7 @@ import ProductCard from "./ProductCard";
 
 const Dashboard = () => {
   return (
-    <div>
+    <div className="d-flex flex-wrap justify-content-center p-3">
       {ProductList.map((ele: any, index: any) => {
         return <ProductCard key={index} product={ele} />;
       })}
--- src/Components/ProductCard.tsx
+++ src/Components/ProductCard.tsx
@@ -6,15 +6,26 @@ interface ProductProps {
 
 const ProductCard = (props: ProductProps) => {
   return (
-    <div className="card m-2">
+    <div className="card m-2" style={{ width: "300px" }}>
       <div>
         <img
           src={props.product.thumbnail}
           height={150}
           width={180}
           alt={props.product.title}
-          className=""
+          className="border-rad mt-2"
         ></img>
+      </div>
+      <div className="card-body">
+        <h5 className="card-title">{props.product.title}</h5>
+        <h6 className="mt-2">Price : {props.product.price}</h6>
+        <h6 className="mt-2">Discount : {props.product.discountPercentage}</h6>
+        <h6 className="mt-2">Rating : {props.product.rating}</h6>
+        {props.product.stock > 0 ? (
+          <button className="btn btn-success">Available</button>
+        ) : (
+          <button className="btn btn-outline-danger">Out of stock</button>
+        )}
       </div>
     </div>
   );
